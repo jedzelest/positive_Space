560341e12da0448644ca89cff9d3a745186c9631
jedzelest <jedzelestrelloso@gmail.com>
2024-08-20 19:06:12 +0800

    second layout in progress, added additional images

--- src/layout/BiographyLayout.tsx
+++ src/layout/BiographyLayout.tsx
@@ -12,7 +12,7 @@ const BiographyLayout = () => {
   return (
     <div className={styles.biography_style}>
       <h3>Biography</h3>
-      <article>
+      <article className={styles.first_layout}>
         <InformationBox
           name={name}
           date_of_birth={date_of_birth}
@@ -26,6 +26,27 @@ const BiographyLayout = () => {
             " I’m Jedzel Estrelloso, an inspiring web developer and a freelance digital illustrator dedicated in creating things that interests me. I graduated April of 2024 under Bachelor of Science in Information Technology.  I have been drawing since middle school or in Grade 7, I have a passion for bringing interesting characters to life. I love both ability to create websites and painting.  "
           }
         </p>
+      </article>
+      <article className={styles.second_layout}>
+        <section className={styles.skills_section}>
+          <div>
+            <img src="src/assets/star.png" alt="" />
+            <h6>Main Skills</h6>
+          </div>
+          <ul>
+            <li>Digital Painting</li>
+            <li>Web Development</li>
+            <li>Concept Art</li>
+            <li>Character Design</li>
+            <li>Color Theory</li>
+            <li>Adobe Photoshop</li>
+            <li>UI/UX Design</li>
+          </ul>
+        </section>
+        <section className={styles.images_section}>
+          <img src="src/assets/Estrelloso_1.jpg" alt="Graduation Pic 1" />
+          <img src="src/assets/Estrelloso_2.jpg" alt="Graduation Pic 2" />
+        </section>
       </article>
     </div>
   );
