70ca9ac8c48cf262e163b8184878de1388113125
jedzelest <jedzelestrelloso@gmail.com>
2025-10-01 15:53:27 +0800

    punctuation corrections

--- src/components/Intro/IntroData.ts
+++ src/components/Intro/IntroData.ts
@@ -1,7 +1,6 @@
 const primary_image = "/assets/Jiezel_center.jpg";
 const openingText = "your guardian angel";
-const paragraph = `"Seasons are changing and waves are crashing
-And stars are falling all for us."`;
+const paragraph = `"Seasons are changing and waves are crashing, and stars are falling all for us."`;
 const author = "The Red Jumpsuit Apparatus";
 
 const links = [
