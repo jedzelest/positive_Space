742a01984fe4f0e299631845433ae39f2a918373
jedzelest <jedzelestrelloso@gmail.com>
2025-10-01 15:40:05 +0800

    changed the opening text

--- src/components/Intro/IntroData.ts
+++ src/components/Intro/IntroData.ts
@@ -1,5 +1,5 @@
 const primary_image = "/assets/Jiezel_center.jpg";
-const openingText = "He has risen, Baby Girl!";
+const openingText = "your guardian angel";
 const paragraph = `"Illustrations can be a big window: a looking glass into the author's
           imagination."`;
 const author = "Emma Lesley";
